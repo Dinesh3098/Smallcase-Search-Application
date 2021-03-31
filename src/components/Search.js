62f6b4f70b428808c1a6d1c52fab11ed6d4f58cf
unknown <bhadanedinesh49@gmail.com>
2021-03-31 21:14:03 +0530

    First commit

---
+++ src/components/Search.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../Style.css'
+class Search extends  React.Component {	
+    constructor( props ) {
+        super( props );		
+        this.state = {
+            query: '',
+            results: {},
+            loading: false,
+            message: '',
+        };	
+    }	
+    render() {
+    return (
+        <div className="container">
+            {/*Heading*/}
+            <h2 className="heading">Live Search: React Application</h2>				{/*Search Input*/}
+            <label className="search-label" htmlFor="search-input">
+                <input
+                    type="text"
+                    value=""
+                    id="search-input"
+                    placeholder="Search..."
+                />
+                <i className="fa fa-search search-icon"/>
+            </label>
+            
+        </div>
+        )
+}
+}export default Search;
